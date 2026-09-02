134dfbf7131bccc16d6a62f931f2b2ab103bbb12
Dana Mohammad <danaalamm8@gmail.com>
2026-09-02 13:07:40 +0300

    Add AppColors theme file

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,23 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryRed=Color(0xffED1C24);
+  static const Color primaryRedDak=Color(0xFFB80710);//when Hover
+
+  static const Color darkBg=Color(0xFF141416);
+  static const Color darkBgGlow=Color(0xFF330C11);
+
+  static const Color textDark=Color(0xFF1E2022);
+  static const Color textMuted=Color(0xFF777E90);
+  static const Color textLight=Colors.white;
+  static const Color labelColor=Color(0xFF353945);
+
+  static const Color scaffoldBg = Color(0xFFF8F9FA);       
+  static const Color cardBg = Colors.white;                
+  static const Color inputFill = Color(0xFFFAFAFA);        
+  static const Color borderLight = Color(0xFFE6E8EC);      
+  static const Color borderFocused = primaryRed;
+  
+}
