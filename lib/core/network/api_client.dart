ae34e0747de75129743aa0ceaf5354561492728d
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-09-07 16:06:34 +0300

    added api constants and api client files

---
+++ lib/core/network/api_client.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:cms_project_app/core/network/api_constants.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class ApiClient {
+  late final Dio _dio;
+
+  ApiClient() {
+    _dio = Dio(
+      BaseOptions(
+        baseUrl: ApiConstants.baseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {'Content-Type': 'application/json', 'Accept': '*/*'},
+      ),
+    );
+    _dio.interceptors.add(
+      PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseBody: true,
+        responseHeader: false,
+        error: true,
+        compact: true,
+        maxWidth: 90,
+      ),
+    );
+  }
+
+  Future<Response> post(String path, {Map<String, dynamic>? data}) async {
+    try {
+      final response = await _dio.post(path, data: data);
+      return response;
+    } on DioException catch (e) {
+      rethrow;
+    }
+  }
+}
