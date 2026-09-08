82eaa491d5af78dcaa8bf1f7a1911dcdb34a3a6e
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-09-08 11:27:09 +0300

    implemented the auth service

---
+++ lib/features/auth/services/auth_service.dart
@@ -0,0 +1,41 @@
+import 'package:dio/dio.dart';
+import 'package:cms_project_app/core/network/api_client.dart';
+import 'package:cms_project_app/core/storage/secure_storage_helper.dart';
+import 'package:cms_project_app/features/auth/models/user_model.dart';
+import 'package:cms_project_app/features/auth/models/sign_in_response_model.dart';
+
+class AuthService {
+  final SecureStorageHelper _storageHelper = SecureStorageHelper();
+  final ApiClient _apiClient = ApiClient();
+
+  Future<UserModel?> signin(String email, String password) async {
+    try {
+      final response = await _apiClient.post(
+        "api/auth/login",
+        data: {"email": email, "password": password},
+      );
+
+      final signInResponse = SignInResponseModel.fromJson(response.data);
+      await _storageHelper.saveToken(signInResponse.token);
+      return signInResponse.user;
+    } on DioException catch (e) {
+      if (e.response?.statusCode == 401) {
+        throw Exception("Invalid email or password.");
+      } else if (e.type == DioExceptionType.connectionTimeout ||
+          e.type == DioExceptionType.receiveTimeout ||
+          e.type == DioExceptionType.connectionError) {
+        throw Exception(
+          "Unable to connect. Please check your internet connection and try again.",
+        );
+      } else if (e.response?.statusCode == 400) {
+        final errorMessage =
+            e.response?.data['error'] ?? "Required fields are missing";
+        throw Exception(errorMessage);
+      } else {
+        throw Exception("Something went wrong. Please try again.");
+      }
+    }
+  }
+
+  // a function for the sign up can be added here.
+}
