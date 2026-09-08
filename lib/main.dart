de84f9f8a59fc1af0875a0f0c920ef53323cebb5
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-09-08 15:57:44 +0300

    hiding the keyboard when pressing outside the textfield or when pressing the button

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
 
-      home: DashboardScreen(),
+      home: SignInScreen(),
     );
   }
 }
