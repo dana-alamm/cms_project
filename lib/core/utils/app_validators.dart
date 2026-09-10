faf5d5cbdc91beee2aa54b579ea88b0ed67aeb0e
AlaaQtishat <alaaqtishat2004@gmail.com>
2026-09-10 15:10:58 +0300

    seperate secure_storage logic from ui

--- lib/core/utils/app_validators.dart
+++ lib/core/utils/app_validators.dart
@@ -22,7 +22,7 @@ class AppValidators {
         !RegExp(r'[A-Z]').hasMatch(val) ||
         !RegExp(r'[a-z]').hasMatch(val) ||
         !RegExp(r'[!@#\$&*~_=%^]+').hasMatch(val)) {
-      return "Password must be at least 8 characters and include at least one uppercase letter, one lowercase letter, and one special character.";
+      return "Password must be at least 8 characters \nand include at least one uppercase letter, one \nlowercase letter, and one special character.";
     }
 
     return null;
